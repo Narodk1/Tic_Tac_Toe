23bd522c6c9be4bee61b332f5e3cf2c193844d06
yousef <youssefnaro@gmail.com>
2023-01-13 00:08:41 +0100

    Main

--- main.cpp
+++ main.cpp
@@ -1,113 +1,230 @@
-/*Simple Project C++ From NARO*/
-#include <iostream>
-#include <stdlib.h>
+/*Simple Project C++ From NARO with two Methods*/
+#include <iostream >
+#include <ctime>
+#include <cstdlib>
+#include <string.h>
 using namespace std;
-//Array for the board
-char board[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
-//Variable Declaration
-int choice;
-int row, column;
-char turn = 'X';
-bool draw = false;
-void display_board() {
-	//Rander Game Board Layout 
-	cout << "PLAYER - 1 [X]\t PLAYER - 2[O] \n";
-	cout << "\t     |     |     \n";
-	cout << "\t  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] <<endl;
-	cout << "\t_____|_____|_____\n";
-	cout << "\t     |     |     \n";
-	cout << "\t  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] <<endl;
-	cout << "\t_____|_____|_____\n";
-	cout << "\t     |     |     \n";
-	cout << "\t  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] <<endl;
-	cout << "\t     |     |     \n";
 
+char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+void display_board();
+void get_x_player_choice();
+void get_o_player_choice();
+void get_computer_choice();
+int board_count(char symbol);
+char check_winner();
+void computer_vs_player();
+void player_vs_player();
+
+void display_board()
+{
+	cout << "Tic Tac Toe From NaroDK";
+	cout << "\t     |     |     \n";
+	cout << "\t  " << board[0] << "  |  " << board[1] << "  |  " << board[2] << endl;
+	cout << "\t_____|_____|_____\n";
+	cout << "\t     |     |     \n";
+	cout << "\t  " << board[3] << "  |  " << board[4] << "  |  " << board[5] << endl;
+	cout << "\t_____|_____|_____\n";
+	cout << "\t     |     |     \n";
+	cout << "\t  " << board[6] << "  |  " << board[7] << "  |  " << board[8] << endl;
+	cout << "\t     |     |     \n";
 }
-void player_turn() {
-	if (turn == 'X') {
-		cout << "\n\tPlayer - 1 [X] turn : ";
+void get_computer_choice()
+{
+	srand(time(0));
+	int choice;
+	do
+	{
+		choice = rand() % 10; /*0-9*/
+	} while (board[choice] != ' ');
+	board[choice] = 'O';
+}
+void get_x_player_choice()
+{
+	while (true)
+	{
+		cout << "select your position (1-9): ";
+		int choice;
+		cin >> choice;
+		choice--; /*the game will be started in 0 that why we need to put -- */
+		if (choice < 0 || choice > 8)
+		{
+			cout << "select your choice from position (1-9):" << endl;
+		}
+		else if (board[choice] != ' ')
+		{
+			cout << "select empty position !" << endl;
+		}
+		else
+		{
+			board[choice] = 'X';
+			break;
+		}
 	}
-	else if (turn == 'O') {
-		cout << "\n\t Player - 2 [O] turn : ";
+}
+void get_o_player_choice()
+{
+	while (true)
+	{
+		cout << "select your position (1-9): ";
+		int choice;
+		cin >> choice;
+		choice--; /*the game will be started in  0 that why we need to put -- */
+		if (choice < 0 || choice > 8)
+		{
+			cout << "select your choice from position (1-9):" << endl;
+		}
+		else if (board[choice] != ' ')
+		{
+			cout << "select empty position !" << endl;
+		}
+		else
+		{
+			board[choice] = 'O';
+			break;
+		}
 	}
-	cin >> choice;
-	//Switch case to get wich row and column will be update
-	switch (choice)
+}
+
+int board_count(char symbol)
+{
+	int total = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		if (board[i] == symbol)
+			total += 1;
+	}
+	return total;
+}
+char check_winner()
+{
+	// horizental*******************************************************
+	if (board[0] == board[1] && board[1] == board[2] && board[0] != ' ')
+		return board[2];
+	if (board[3] == board[4] && board[4] == board[5] && board[3] != ' ')
+		return board[4];
+	if (board[6] == board[7] && board[7] == board[8] && board[6] != ' ')
+		return board[7];
+	// vertical***************************************************
+	if (board[0] == board[3] && board[3] == board[6] && board[0] != ' ')
+		return board[0];
+	if (board[1] == board[5] && board[5] == board[7] && board[1] != ' ')
+		return board[1];
+	if (board[2] == board[5] && board[5] == board[8] && board[2] != ' ')
+		return board[2];
+	//\/**************************************************************
+	if (board[0] == board[4] && board[4] == board[4] == board[8] && board[0] != ' ')
+		return board[0];
+	if (board[2] == board[4] && board[4] == board[6] && board[2] != ' ')
+		return board[2];
+	if (board_count('X') + board_count('O'))
+		return 'c';
+	else
+		return 'd';
+}
+void computer_vs_player()
+{
+	string playerN;
+	cout << "entrer Votre nom : ";
+	cin >> playerN;
+	while (true)
+	{
+		system("cls");
+		display_board();
+		if (board_count('X') == board_count('O'))
+		{
+			cout << "c'est le tour de : " << playerN << endl;
+			get_x_player_choice();
+		}
+		else
+		{
+			get_computer_choice();
+		}
+		char winner = check_winner();
+		if (winner == 'X')
+		{
+			system("cls");
+			display_board();
+			cout << playerN << " a gagner cette partie!!" << endl;
+			break;
+		}
+		else if (winner == 'O')
+		{
+			system("cls");
+			display_board();
+			cout << " J'ai gagner cette partie!!" << endl;
+			break;
+		}
+		else if (winner == 'd')
+		{
+			cout << "la partire est terminer!!" << endl;
+			break;
+		}
+	}
+}
+void player_vs_player()
+{
+	string playerX, playerO;
+	cout << "entrer le nom de joueur X: ";
+	cin >> playerX;
+	cout << "entrer le nom de joueur Y: ";
+	cin >> playerO;
+	while (true)
+	{
+		system("cls");
+		display_board();
+		if (board_count('X') == board_count('O'))
+		{
+			cout << "c'est le tour de : " << playerX << endl;
+			get_x_player_choice();
+		}
+		else
+		{
+			cout << "c'est le tour de : " << playerO << endl;
+			get_o_player_choice();
+		}
+		char winner = check_winner();
+		if (winner == 'X')
+		{
+			system("cls");
+			display_board();
+			cout << playerX << " a gagner cette partie!!" << endl;
+			break;
+		}
+		else if (winner == 'O')
+		{
+			system("cls");
+			display_board();
+			cout << playerO << " a gagner cette partie!!" << endl;
+			break;
+		}
+		else if (winner == 'd')
+		{
+			cout << "la partie est terminer!!" << endl;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int mode;
+	cout << "1- Computer VS Player." << endl;
+	cout << "2- Player VS Player." << endl;
+	cout << "selectionner le mode de jeux: ";
+	cin >> mode;
+	switch (mode)
 	{
 	case 1:
-		row = 0; column = 0; break;
+		computer_vs_player();
+		break;
 	case 2:
-		row = 0; column = 1; break;
-	case 3:
-		row = 0; column = 2; break;
-	case 4:
-		row = 1; column = 0; break;
-	case 5:
-		row = 1; column = 1; break;
-	case 6:
-		row = 1; column = 2; break;
-	case 7:
-		row = 2; column = 0; break;
-	case 8:
-		row = 2; column = 1; break;
-	case 9:
-		row = 2; column = 2; break;
+		player_vs_player();
+		break;
 	default:
-		cout << "Invalid Move";
+		cout << "SVP Choisi les options disponible dans le menu!!" << endl;
+		cout << "choisi encore le mode de jeux: ";
+	   cin >> mode;
+	break;
 	}
-	if (turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
-		//Updating The Position For 'X' Symbol if
-		//Positio Is Not AlReady Occupied
-		board[row][column] = 'X';
-	    turn = 'O';
-	}
-	else if (turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O') {
-		//Updating The Position For 'O' Symbol if
-		//Positio Is Not AlReady Occupied
-		board[row][column] = 'O';
-		turn = 'X';
-	}
-	else {
-		cout << "Box Is AlReady Is Filled \n Please Choose Another Choice  " << endl;
-		player_turn();
-	}
-	/*Display board for show Result*/
-	display_board();
-}
-//Function to get Game Status 
-bool gameover() {
-	for (int i = 0; i < 3; i++) 
-		//checking the win for both diagnol /row and column
-		if (board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[i][0] == board[1][i] && board[0][i] == board[2][i])
-			return false;
-		if (board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
-			return false;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (board[i][j] != 'X' && board[i][j] != 'O')
-					return true;
-				    draw = true;
-			}
-		}
-		return false;
-}
-/*main*/
-int main() {
-	cout << "TIC TAC TOE GAME FROM N-A-R-O" << endl;
-	cout << "For Two Players "<<endl;
-	while (gameover())
-	{
-		display_board();
-		player_turn();
-		gameover();
-	}
-	if (turn == 'X' && draw == false) {
-		cout << "\n Congratulation ! Player With 'X' has won the game";
-	}
-	else if (turn == 'O' && draw == false) {
-		cout<<"\n Congratulation ! Player With 'O' has won the game";
-	}
-	else {
-		cout << "\n GAME DRAW!!!\n";
-	}
+	return 0;
 }
